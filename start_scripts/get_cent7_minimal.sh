23384c61183bdb5b9d88284815790a3c3f8df906
Steve Louie <stephen.r.louie@gmail.com>
2016-03-16 15:50:10 -0400

    Added the centos 7 image

---
+++ start_scripts/get_cent7_minimal.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+
+vagrant box add cent7_minimal https://github.com/holms/vagrant-centos7-box/releases/download/7.1.1503.001/CentOS-7.1.1503-x86_64-netboot.box 
--- start_scripts/start.sh
+++ start_scripts/start.sh
@@ -1,8 +1,9 @@
 kube_version=1.1.4
 flannel_version=0.5.5
 
-./get_cent6_minimal.sh
-./get_coreos_pxe.sh
+#./get_cent6_minimal.sh
+./get_cent7_minimal.sh
+#./get_coreos_pxe.sh
 
-kube_version=$kube_version ./get_kube.sh
-flannel_version=$flannel_version ./get_flannel.sh
+#kube_version=$kube_version ./get_kube.sh
+#flannel_version=$flannel_version ./get_flannel.sh
